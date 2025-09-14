bf0fe0fd36660e4dbbd35f06b254bc882fe279b9
wismini2002 <wisminid@gmail.com>
2025-09-14 22:21:02 +0530

    q1

--- src/LW_01/Q_01.java
+++ src/LW_01/Q_01.java
@@ -1,7 +1,19 @@
 package LW_01;
 
+import java.util.*;
 public class Q_01 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Scanner scanner=new Scanner(System.in);
+        String word;
+        char middle;
+        System.out.println("Enter a word of odd length.");
+        word=scanner.next();
+        if(word.length()%2==0){
+            System.out.println("This is not an odd length word.");
+        }
+        else{
+            middle=word.charAt(word.length()/2);
+            System.out.println("Middle Character: "+middle);
+        }
     }
 }
