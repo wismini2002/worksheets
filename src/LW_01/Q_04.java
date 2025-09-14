7e617c6ae30d6cf55877100080a971cb803d7ff7
wismini2002 <wisminid@gmail.com>
2025-09-14 23:43:45 +0530

    q4

---
+++ src/LW_01/Q_04.java
@@ -0,0 +1,12 @@
+package LW_01;
+import javax.swing.*;
+public class Q_04 {
+    public static void main(String[] args) {
+        JFrame myWindow;
+        myWindow=new JFrame();
+        myWindow.setTitle("My First Frame");
+        myWindow.setSize(300,200);
+        myWindow.setLocation(100,50);
+        myWindow.setVisible(true);
+    }
+}
