1f2de77f5133d82611b5b2bdb7e805a2367d978c
wismini2002 <wisminid@gmail.com>
2025-10-29 17:58:42 +0530

    q1

--- src/LW_02/Q_01.java
+++ src/LW_02/Q_01.java
@@ -1,5 +1,18 @@
 package LW_02;
 
 public class Q_01 {
-    
+    public static void main(String[] args) {
+        int first=10;
+        int last=49;
+        int count=0;
+
+        for(int i=first;i<=last;i++){
+            System.out.print(i+" ");
+            count++;
+
+            if(count%10==0){
+                System.out.println();
+            }
+        }
+    }
 }
