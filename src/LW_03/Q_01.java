4b039d5289d19de3b0c8355fddfd111de127320a
wismini2002 <wisminid@gmail.com>
2026-01-20 00:16:29 +0530

    q1 done

--- src/LW_03/Q_01.java
+++ src/LW_03/Q_01.java
@@ -1,5 +1,29 @@
 package LW_03;
 
-public class Q_01 {
+import java.util.Scanner;
 
+public class Q_01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        Temperature temperature = new Temperature();
+
+        System.out.println("Enter 'c' for Celsius and 'f' for Fahrenheit in Temperature ?");
+        String temp = input.next();
+
+        if(temp.equals("c") || temp.equals("C")){
+            System.out.println("Enter the temperature in  Celsius: ");
+            temperature.setCelsius(input.nextDouble());
+            System.out.println("Temperature is "+temperature.toFahrenheit()+"F");
+        }
+        else if(temp.equals("f") || temp.equals("F")){
+            System.out.println("Enter the temperature in  Fahrenheit: ");
+            temperature.setFahrenheit(input.nextDouble());
+            System.out.println("Temperature is "+temperature.toCelsius()+"C");
+        }
+        else {
+            System.out.println("Wrong Input");
+        }
+
+    }
 }
